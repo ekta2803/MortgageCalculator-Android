0522306d83ab7de708a01290e182f76e429da3f2
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-03-14 18:04:18 -0700

    Property Info has search ui

--- app/src/main/java/com/gcekta/mortgagecalculator/activities/PropertyInfoActivity.java
+++ app/src/main/java/com/gcekta/mortgagecalculator/activities/PropertyInfoActivity.java
@@ -2,6 +2,11 @@ package com.gcekta.mortgagecalculator.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +17,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.gcekta.mortgagecalculator.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class PropertyInfoActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,6 +34,9 @@ public class PropertyInfoActivity extends AppCompatActivity
 
     private FloatingActionButton fab;
     private Spinner propertyType;
+    private TextInputLayout streetAddressLayout, zipLayout;
+    private EditText streetAddress, zip;
+    private AutoCompleteTextView state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +56,23 @@ public class PropertyInfoActivity extends AppCompatActivity
 //        };
 //        fab.setOnClickListener(fabListener);
 
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -60,7 +91,55 @@ public class PropertyInfoActivity extends AppCompatActivity
         propTypeAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         propertyType.setAdapter(propTypeAdapter);
 
+        streetAddressLayout = (TextInputLayout) findViewById(R.id.street_text_layout);
+        streetAddress = (EditText) findViewById(R.id.street_text);
+
+        state = (AutoCompleteTextView) findViewById(R.id.state_text);
+        ArrayAdapter<CharSequence> stateAdapter = ArrayAdapter.createFromResource(this,
+                R.array.statearray,
+                android.R.layout.simple_list_item_1);
+        state.setAdapter(stateAdapter);
+        state.setThreshold(1);
+
+        zipLayout = (TextInputLayout) findViewById(R.id.zip_text_layout);
+        zip = (EditText) findViewById(R.id.zip_text);
+
+
         //Text Field listeners
+        TextWatcher streetAddressWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        TextWatcher zipWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
 
 
         //add All listeners
@@ -76,7 +155,7 @@ public class PropertyInfoActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-    
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
